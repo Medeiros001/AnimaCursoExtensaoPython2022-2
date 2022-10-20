f9eb9ed539cd13c4ab6ae60b0b8d6039fbc46e09
Ana Paula De Medeiros <90284746+Medeiros001@users.noreply.github.com>
2022-10-20 00:53:17 +0000

    Aprendi a criar variáveis.

--- main.py
+++ main.py
@@ -3,6 +3,14 @@
 # print() = comando de saida
 print("Alo mundo!")
 
+# Quando quiser guardar uma String! (frase). O sinal de = é pra guradar variável.
+nome = "Ana Paula"
+# Quando quiser guardar um número inteiro.
+idade = 37
+
+#Exibir o nome (que está dentro da cariável nome)
+print(nome)
+
 
 '''
 Comentario em bloco
