c35a8a6b885dd5f5f3d87104427ca0466bf16053
Ana Paula De Medeiros <90284746+Medeiros001@users.noreply.github.com>
2022-10-20 01:18:22 +0000

    Aprendemos a exibir de três maneiras diferentes.

--- main.py
+++ main.py
@@ -11,6 +11,20 @@ idade = 37
 #Exibir o nome (que está dentro da cariável nome)
 print(nome)
 
+#Quando quiser exibir a frase "Minha idade é " completando com o conteúdo da variável idade.
+#str é uma conversão de inteiro para string
+print("Minha idade é "+str(idade)+" anos")
+#outra forma é; colocar o f de "formatação"
+print(f"Minha idade é {idade}\n")
+print("Minha idade é {}".format(idade))
+
+#Quando quiser exibir "Meu nome é... e tenho ... anos..." trocando pelas variáveis nome e idade.
+print("meu nome é {} e tenho {} anos". format(nome,idade))
+
+
+
+
+
 
 '''
 Comentario em bloco
