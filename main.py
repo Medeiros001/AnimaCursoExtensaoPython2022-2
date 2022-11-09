a8816ef036e861b3e29584e8798daf1c8cdbac3f
Ana Paula De Medeiros <90284746+Medeiros001@users.noreply.github.com>
2022-11-09 19:06:55 +0000

    Aprendi a criar variáveis e a exibi-la na tela!

--- main.py
+++ main.py
@@ -3,7 +3,13 @@
 # print() = comando de saida
 print("Alo Mundo!")
 
+# Quando quiser guardar um String! (frase)
+nome = "Ana Paula Medeiros"
+# Quando quiser guardar um número inteiro
+idade: 37
 
+# Exibir o meu nome (que está dentro da variável nome)
+print(nome)
 
 '''
 Comentario em bloco
