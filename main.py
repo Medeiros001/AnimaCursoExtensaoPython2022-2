20b32bbff969d3dda05bacc3dc6c30d06bb1d586
Ana Paula De Medeiros <90284746+Medeiros001@users.noreply.github.com>
2022-11-09 19:22:33 +0000

    Aprendemos a exibir de três maneiras diferentes!

--- main.py
+++ main.py
@@ -6,10 +6,23 @@ print("Alo Mundo!")
 # Quando quiser guardar um String! (frase)
 nome = "Ana Paula Medeiros"
 # Quando quiser guardar um número inteiro
-idade: 37
+idade = 37
 
 # Exibir o meu nome (que está dentro da variável nome)
 print(nome)
+
+# Quando quiser exibir a frase "Minha idade é " completando com o conteúdo da variável idade
+#print("Meu nome é "+nome)
+print("Minha idade é "+str(idade))
+print(f"Minha idade é {idade}\n")
+print("Minha idade é {}". format(idade))
+
+# Quando quiser eibir "Meu nome é ... e tenho ... anos.." trocando pelas variáveis nome e idade
+print("Meu nome é {} e tenho {} anos". format(nome,idade))
+
+
+
+
 
 '''
 Comentario em bloco
